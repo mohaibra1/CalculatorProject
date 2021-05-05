660f376184313d33d0179e3f7ca331b852dda4f2
mimoh <mimoh@DESKTOP-I23K3L6>
2021-05-05 11:58:38 +0200

    eerste wijziging

--- calculator.vb
+++ calculator.vb
@@ -1,1 +1,29 @@
+Imports System
 
+Public Class Rectangle
+    Private length As Double
+    Private width As Double
+
+    'Public Methods
+    Public Sub AcceptDetails()
+        lenght = 4.5
+        width = 3.5
+    End Sub
+
+    Public Function GetArea() As Double
+        GetArea = length * width
+    End Function
+
+    Public Sub Display()
+        Console.WriteLine("Length: {0}", length)
+        Console.WriteLine("Width: {0}", width)
+        Console.WriteLine("Area: {0}", GetArea())
+    End Sub
+
+    Shared Sub Main()
+        Dim r As Rectangle()
+        r.AcceptDetails()
+        r.Display()
+        Console.ReadLine()
+    End Sub
+End Class
